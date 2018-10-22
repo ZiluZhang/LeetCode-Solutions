1505ae5fb562deff542a9c88205f79281d78a8fe
ZiluZhang <zhangzilu@pku.edu.cn>
2018-10-22 00:02:23 -0700

    start top fb questions tag

--- 028.cpp
+++ 028.cpp
@@ -1,22 +1,42 @@
+// Implement strStr()
+// mystring.compare(mystring2)返回值是0说明一样
+// mystring.substr(start, len)取子串
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
         int len1 = haystack.length();
         int len2 = needle.length();
-        if(len2 == 0)
-            return 0;
-        if(len1 == 0)
-            return -1;
-        for(int i = 0; i < len1; i++)
-        {
-            if(i+len2 > len1)
-                break;
-            if(needle.compare(haystack.substr(i,len2)) == 0)
-                return i;
+        if(len2 == 0) return 0;
+        if(len1 == 0) return -1;
+        if(len1 < len2) return -1;
+        for(int i = 0; i < len1; i++) {
+            if(i + len2 > len1) break;
+            if(needle.compare(haystack.substr(i, len2)) == 0) return i;
         }
-        cout << needle.compare(haystack.substr(2,2+len2)) << endl;
-        cout << needle << endl;
-        cout << haystack.substr(2,2+len2) << endl;
         return -1;
     }
 };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+//         int len1 = haystack.length();
+//         int len2 = needle.length();
+//         if(len2 == 0)
+//             return 0;
+//         if(len1 == 0)
+//             return -1;
+//         for(int i = 0; i < len1; i++)
+//         {
+//             if(i+len2 > len1)
+//                 break;
+//             if(needle.compare(haystack.substr(i,len2)) == 0)
+//                 return i;
+//         }
+//         cout << needle.compare(haystack.substr(2,2+len2)) << endl;
+//         cout << needle << endl;
+//         cout << haystack.substr(2,2+len2) << endl;
+//         return -1;
+//     }
+// };
--- 069.cpp
+++ 069.cpp
@@ -1,19 +1,39 @@
+// Sqrt(x)
+// 二分法查，最后return right
+// 防止越界，不要用乘法
+
+
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x <= 1)
-            return x;
-        int l = 0, r = x;
-        while(l <= r)
-        {
-            int m = l + (r-l)/2;
-            if(m == x/m)
-                return m;
-            else if(m < x/m)
-                l = m+1;
-            else if(m > x/m)
-                r = m-1;
+        if(x <= 1) return x;
+        int left = 1, right = x-1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(x / mid == mid) return mid;
+            else if(x / mid > mid) left = mid + 1;
+            else right = mid - 1;
         }
-        return r;
+        return right;
     }
 };
+
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         if(x <= 1)
+//             return x;
+//         int l = 0, r = x;
+//         while(l <= r)
+//         {
+//             int m = l + (r-l)/2;
+//             if(m == x/m)
+//                 return m;
+//             else if(m < x/m)
+//                 l = m+1;
+//             else if(m > x/m)
+//                 r = m-1;
+//         }
+//         return r;
+//     }
+// };
